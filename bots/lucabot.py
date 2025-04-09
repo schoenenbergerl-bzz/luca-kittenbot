03489f5614fee21a04f723b30031d86a17eb50d3
lucas <schoenenbergerl@bzz.ch>
2025-04-09 15:49:48 +0200

    with bot

--- bots/lucabot.py
+++ bots/lucabot.py
@@ -1,83 +1,81 @@
-import json
 import random
+from typing import List, Optional
+
+from bot import Bot
+from card import Card, CardType
+from game_handling.game_state import GameState
 
 
-class TemplateBot():
-    """
-    Enhanced TemplateBot with strategies to handle Exploding Kittens.
-    """
+class SmartBot(Bot):
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.future_cards = []
 
-    def __init__(self, name):
-        self.name = name
-        self._bot = SmarterKitten(name)
-
-    def request(self, data):
+    def play(self, state: GameState) -> Optional[Card]:
         """
-        Handle requests for different actions in the game.
+        Decide whether to play a card or skip based on game state and strategy.
+        Prioritize "See the Future" cards and conserve others unless hand is large.
+
+        :param state: GameState object
+        :return: Card object or None
         """
-        payload = json.loads(data)
-        action = payload['action']
-        if action == 'PLAY':
-            return self._bot.play()
-        elif action == 'START':
-            return self._bot.start_round()
-        elif action == 'DRAW':
-            return self._bot.add_card(payload['card'])
-        elif action == 'INFORM':
-            return self._bot.inform(payload['botname'], payload['event'], payload['data'])
-        elif action == 'DEFUSE':
-            return self._bot.defuse(payload['decksize'])
-        elif action == 'FUTURE':
-            return self._bot.see_the_future(payload['cards'])
+        playable_cards = [card for card in self.hand if card.card_type != CardType.DEFUSE]
+        if not playable_cards:
+            return None
+
+        see_future_cards = [card for card in playable_cards if card.card_type == CardType.SEE_THE_FUTURE]
+
+        if see_future_cards and not self.future_cards:
+            return see_future_cards[0]
+
+        if self.future_cards and self.future_cards[0].card_type == CardType.EXPLODING_KITTEN:
+            return random.choice(playable_cards)
+
+        if len(self.hand) > 5:
+            return random.choice(playable_cards)
+
         return None
 
+    def handle_exploding_kitten(self, state: GameState) -> int:
+        """
+        Place the Exploding Kitten strategically in the draw pile.
+        Prefer deeper placement to delay its reappearance.
 
-class SmarterKitten():
-    """
-    This class contains the strategic implementation of the TemplateBot.
-    """
+        :param state: GameState object
+        :return: int index of the draw pile
+        """
+        deck_size = state.cards_left_to_draw
+        if deck_size <= 1:
+            return 0
 
-    def __init__(self, name):
-        self.name = name
-        self._hand = []
-        self._future_cards = []
+        if self.future_cards:
+            safe_spots = list(range(deck_size))
+            for i, card in enumerate(self.future_cards[:min(3, deck_size)]):
+                if card.card_type == CardType.EXPLODING_KITTEN and i in safe_spots:
+                    safe_spots.remove(i)
+            if safe_spots:
+                return random.choice(safe_spots[-len(safe_spots) // 5:])
 
-    def start_round(self):
-        self._hand = []
-        self._future_cards = []
-        return 'ACK'
+        return random.randint(max(0, deck_size - deck_size // 5), deck_size - 1)
 
-    def add_card(self, cardname):
-        self._hand.append(cardname)
-        return 'ACK'
+    def see_the_future(self, state: GameState, top_three: List[Card]) -> None:
+        """
+        Store the top three cards seen to inform future decisions.
 
-    def inform(self, botname, event, data):
-        return 'ACK'
+        :param state: GameState object
+        :param top_three: List of top three cards of the draw pile
+        :return: None
+        """
+        self.future_cards = top_three.copy()
 
-    def play(self):
-        if self._future_cards and self._future_cards[0] == 'EXPLODING_KITTEN':
-            if 'SKIP' in self._hand:
-                self._hand.remove('SKIP')
-                self._future_cards = []
-                return 'SKIP'
-            elif 'SHUFFLE' in self._hand:
-                self._hand.remove('SHUFFLE')
-                self._future_cards = []
-                return 'SHUFFLE'
+    def card_played(self, card_type: CardType, position: int) -> bool:
+        """
+        React to another bot playing a card. Track "See the Future" usage.
 
-        if self._hand:
-            if 'NORMAL' in self._hand:
-                return self._hand.pop()
-            return self._hand.pop(0)
-        return None
-
-    def defuse(self, decksize):
-        if 'EXPLODING_KITTEN' in self._hand:
-            self._hand.remove('EXPLODING_KITTEN')
-            position = random.randint(0, decksize)
-            return position
-        return 0
-
-    def see_the_future(self, top_three):
-        self._future_cards = top_three
-        return 'ACK'
+        :param card_type: CardType object
+        :param position: int
+        :return: bool
+        """
+        if card_type == CardType.SEE_THE_FUTURE:
+            self.future_cards = []
+        return True
